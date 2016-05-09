714ebbac29952e0a2faaaaa5b8413deece44c0e7
이서현 <@192.168.25.52>
2016-05-09 23:11:26 +0900

    하하
    됐다

--- src/해보자project/해봐.java
+++ src/해보자project/해봐.java
@@ -5,7 +5,7 @@ public class 해봐 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("!!");
-		System.out.println("아 배가 부글거려");
+		System.out.println("아 배가 부글거렼ㅋㅋㅋㅋㅋㅋ");
 		
 	}
 
